2f87f2b4b10b76989252c5eef972a6a67f1f19c3
paryginaad <paryginaad@PC-H2A310W10>
2024-05-15 11:13:38 +0300

    Add project documentation.

---
+++ Parygina_Word_Doc/html/search/functions_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['parygina_5fclass_5fword',['Parygina_Class_Word',['../class_parygina___class___word.html#af1ea600ae9467ee35438fbafe6786d02',1,'Parygina_Class_Word::Parygina_Class_Word(void)'],['../class_parygina___class___word.html#aec6be6c40a73df07bb51f37b670b23c8',1,'Parygina_Class_Word::Parygina_Class_Word(char *word, int PageNumber, char *NumberOfPages)']]]
+];
