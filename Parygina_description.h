8045720e1d0cdc806556603107869463416334de
paryginaad <paryginaad@PC-H2A310W10>
2024-05-15 10:43:45 +0300

    Add project description file.

---
+++ Parygina_description.h
@@ -0,0 +1,13 @@
+/**
+	@mainpage Проект LR3 для тестирования DoxyGen
+	Используется в лабораторной работе номер 3
+	по курсу Проектирование программного обеспечения
+	@copyright	Вогу
+	@author		Парыгина А.Д.
+	@date		15-05-2024
+\par Использует классы:
+- @ref Parygina_Class_Word
+\par Использует классы:
+- @ref Parygina_Class_Word.h
+- @ref Parygina_Class_Word.cpp
+*/
