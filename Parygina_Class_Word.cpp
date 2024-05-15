2e10d5ce914e5d6cbd56a692e7bba790c74b550e
paryginaad <paryginaad@PC-H2A310W10>
2024-05-15 10:42:09 +0300

    Add project code file.

---
+++ Parygina_Class_Word.cpp
@@ -0,0 +1,23 @@
+#include "stdafx.h"
+#include "Parygina_Class_Word.h"
+
+/**
+	@file	Parygina_Class_Word.cpp
+	@brief	Файл исходных кодов класса Word
+	@copyright	Вогу
+	@author		Парыгина А.Д.
+	@date		15-05-2024
+	@version	1.0.0
+\par Использует классы:
+	@ref Parygina_Class_Word.h
+\par Содержит класс:
+	@ref Parygina_Class_Word
+*/
+
+Parygina_Class_Word::Parygina_Class_Word()
+{
+}
+
+Parygina_Class_Word::~Parygina_Class_Word()
+{
+}
