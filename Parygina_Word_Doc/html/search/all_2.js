6007dd23302258942b99a50cc5e3f40fe2c1925c
Анастасия Парыгина <124684037+ParyginaAnastasiya@users.noreply.github.com>
2024-05-16 22:53:01 +0300

    Delete Parygina_Word_Doc directory

--- Parygina_Word_Doc/html/search/all_2.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['parygina_5fclass_5fword',['Parygina_Class_Word',['../class_parygina___class___word.html',1,'Parygina_Class_Word'],['../class_parygina___class___word.html#af1ea600ae9467ee35438fbafe6786d02',1,'Parygina_Class_Word::Parygina_Class_Word(void)'],['../class_parygina___class___word.html#aec6be6c40a73df07bb51f37b670b23c8',1,'Parygina_Class_Word::Parygina_Class_Word(char *word, int PageNumber, char *NumberOfPages)']]],
-  ['parygina_5fclass_5fword_2ecpp',['Parygina_Class_Word.cpp',['../_parygina___class___word_8cpp.html',1,'']]],
-  ['parygina_5fclass_5fword_2eh',['Parygina_Class_Word.h',['../_parygina___class___word_8h.html',1,'']]]
-];
--- Parygina_Word_Doc/html/search/functions_3.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['_7eparygina_5fclass_5fword',['~Parygina_Class_Word',['../class_parygina___class___word.html#a9006eff4845ea14e70a9bc7ba398d758',1,'Parygina_Class_Word']]]
-];
